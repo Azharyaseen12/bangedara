22e8282e9d0f353c00b9fe239ca10d090508b0a5
Azharyaseen12 <swen19111105@kfueit.edu.pk>
2025-07-12 14:26:40 +0500

    eye button added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -48,9 +48,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({
         />
         {rightIcon && (
           <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-            <div className="text-gray-400">
-              {rightIcon}
-            </div>
+            {rightIcon}
           </div>
         )}
       </div>
